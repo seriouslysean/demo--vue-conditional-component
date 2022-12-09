da7fdeda951cb211d714dc61c60c9b54fc23ec21
seriouslysean <sean@codeserious.com>
2022-12-09 10:24:23 -0500

    Add h explanation link

--- src/components/ConditionalRenderWrapper.js
+++ src/components/ConditionalRenderWrapper.js
@@ -8,6 +8,8 @@ export default {
   },
   // Vue 3 Render Functions, https://vuejs.org/api/render-function.html#h
   // For Vue 2, see https://v2.vuejs.org/v2/guide/render-function.html#ad
+  // Also relevant: What does the `h` stand for?
+  // See https://css-tricks.com/what-does-the-h-stand-for-in-vues-render-method/
   render() {
     if (!this.wrap) {
       return h(this.$slots.default);
